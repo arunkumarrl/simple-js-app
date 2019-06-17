1b17df87dc5cadc625b9dcb5976f70dc456d900a
arunkumarrl <arunkumar_rl@yahoo.com>
2019-06-17 15:59:11 +0200

    Task with bonus task 1
    pokemon app, and then conditional loops were applied along with css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
-alert('Hello world');
+var repository=[];
+var pokemon1={
+  name:'Bulbasaur',
+  height:7,
+  types:['grass','poison']
+};
+var pokemon2={
+  name:'Squirtle',
+  height:0.5,
+  types:['grass','electric']
+};
+var pokemon3={
+  name:'Pikachu',
+  height:0.4,
+  types:['ground']
+};
+repository=[pokemon1,pokemon2,pokemon3];
+for (var i = 0; i < repository.length; i++) {
+  if (repository[i].types[0]=='grass') {
+    if (repository[i].height>1) {
+      //document.write("<span style='color:green'>"+repository[i].name+' (height:'+repository[i].height+')-Wow, thats big!)<br>'+"</span>");
+      document.write("<span class='plant'>"+repository[i].name+' (height:'+repository[i].height+')-Wow, thats big!)<br>'+"</span>");
+    }
+    else {
+      //document.write("<span style='color:green'>"+repository[i].name+' (height:'+repository[i].height+')<br>'+"</span>");
+      document.write("<span class='plant'>"+repository[i].name+' (height:'+repository[i].height+')<br>'+"</span>");
+    }
+  }
+  else {
+    //document.write("<span style='color:red'>"+repository[i].name+' (height:'+repository[i].height+')<br>'+"</span>");
+    document.write("<span class='fire'>"+repository[i].name+' (height:'+repository[i].height+')<br>'+"</span>");
+  }
+}
