d4745ba1fbfe6c09fdcff2285839797793095976
arunkumarrl <arunkumar_rl@yahoo.com>
2019-06-23 01:01:38 +0200

    changes made in foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,8 @@ var repository=[
   height:0.4,
   types:['ground']
 }]
-for (var i = 0; i < repository.length; i++) {
-  /*if (repository[i].types[0]=='grass') {
+/*for (var i = 0; i < repository.length; i++) {
+  if (repository[i].types[0]=='grass') {
     if (repository[i].height>1) {
       //document.write("<span style='color:green'>"+repository[i].name+' (height:'+repository[i].height+')-Wow, thats big!)<br>'+"</span>");
       document.write("<span class='plant'>"+repository[i].name+' (height:'+repository[i].height+')-Wow, thats big!<br>'+"</span>");
@@ -28,9 +28,10 @@ for (var i = 0; i < repository.length; i++) {
   else {
     //document.write("<span style='color:red'>"+repository[i].name+' (height:'+repository[i].height+')<br>'+"</span>");
     document.write("<span class='fire'>"+repository[i].name+' (height:'+repository[i].height+')<br>'+"</span>");
-  }*/
+  }
 repositoryPrint(repository[i]);
-}
+}*/
 function repositoryPrint(repository){
   document.write('(name:'+repository.name+') (height:'+repository.height+') (types:'+repository.types+')<br>');
 }
+repository.forEach(repositoryPrint);
