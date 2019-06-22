64d198cdcef3fe4c55ad6708f51d3f870c9391b5
arunkumarrl <arunkumar_rl@yahoo.com>
2019-06-23 00:41:29 +0200

    Task 1.4 using forEach loop
    forEachloop was used to print the details of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
-var repository=[];
-var pokemon1={
+var repository=[
+{
   name:'Bulbasaur',
   height:7,
   types:['grass','poison']
-};
-var pokemon2={
+},
+{
   name:'Squirtle',
   height:0.5,
   types:['grass','electric']
-};
-var pokemon3={
+},
+{
   name:'Pikachu',
   height:0.4,
   types:['ground']
-};
-repository=[pokemon1,pokemon2,pokemon3];
+}]
 for (var i = 0; i < repository.length; i++) {
-  if (repository[i].types[0]=='grass') {
+  /*if (repository[i].types[0]=='grass') {
     if (repository[i].height>1) {
       //document.write("<span style='color:green'>"+repository[i].name+' (height:'+repository[i].height+')-Wow, thats big!)<br>'+"</span>");
       document.write("<span class='plant'>"+repository[i].name+' (height:'+repository[i].height+')-Wow, thats big!<br>'+"</span>");
@@ -29,5 +28,9 @@ for (var i = 0; i < repository.length; i++) {
   else {
     //document.write("<span style='color:red'>"+repository[i].name+' (height:'+repository[i].height+')<br>'+"</span>");
     document.write("<span class='fire'>"+repository[i].name+' (height:'+repository[i].height+')<br>'+"</span>");
-  }
+  }*/
+repositoryPrint(repository[i]);
+}
+function repositoryPrint(repository){
+  document.write('(name:'+repository.name+') (height:'+repository.height+') (types:'+repository.types+')<br>');
 }
