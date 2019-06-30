5148c6b17d17c849efccbd51f18e59e6f0d8a391
arunkumarrl <arunkumar_rl@yahoo.com>
2019-06-30 22:10:05 +0200

    Task 1.5-IIFE used
    getAll, add, forEach,Object.keys(),filter()

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,62 @@
-var repository=[
-{
-  name:'Bulbasaur',
-  height:7,
-  types:['grass','poison']
-},
-{
-  name:'Squirtle',
-  height:0.5,
+//repository.forEach (function(element){document.write('(name:'+element.name+') (height:'+element.height+') (types:'+element.types+')<br>');});
+var pokemonRepository=(function () {
+  var repository=[
+  {
+    name:'Bulbasaur',
+    height:7,
+    types:['grass','poison']
+  },
+  {
+    name:'Squirtle',
+    height:0.5,
+    types:['grass','electric']
+  },
+  {
+    name:'Pikachu',
+    height:0.4,
+    types:['ground']
+  }];
+  return {
+    add: function(pokemon) {
+      repository.push(pokemon);
+    },
+    getAll: function() {
+      return repository;
+    },
+    filterPokemon: function(){
+    return repository.filter(element=>(element.name==='Pikachu'));//Bonus task filter function
+    }
+  };
+})();
+//console.log(pokemonRepository.filterPokemon());
+//Bonus task 1, checking if the parameter is an object
+var item1={
+  name:'Xatu',
+  height:4.11,
+  types:['psychic','flying']
+};
+var item2="Arun";
+var item3=4;
+var test2=[item1,item2,item3];
+test2.forEach(function(element){
+  if(typeof(element)==="object"){
+    pokemonRepository.add(element);
+  }
+});
+//To compare the keys of the object
+var repositoryKeys=(Object.keys(pokemonRepository.getAll()[0]));
+var item4={
+  name:'Poliwag',
+  height:0.6,
   types:['grass','electric']
-},
-{
-  name:'Pikachu',
-  height:0.4,
-  types:['ground']
-}]
-repository.forEach (function(element){document.write('(name:'+element.name+') (height:'+element.height+') (types:'+element.types+')<br>');});
+};
+var test3=(Object.keys(item4));
+if(test3.length===3){
+  if (test3[0]===repositoryKeys[0] && test3[1]===repositoryKeys[1] && test3[2]===repositoryKeys[2]){
+    pokemonRepository.add(item4);
+  }
+}
+var test=(pokemonRepository.getAll());
+test.forEach (function(element){
+document.write('(name:'+element.name+') (height:'+element.height+') (types:'+element.types+')<br>');
+});
